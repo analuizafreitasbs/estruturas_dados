26a736f0880023246c9f2d658cd84e106bcb6aab
analuizafreitasbs <sanaluizafreitas@gmail.com>
2025-04-27 17:50:14 -0300

    final

---
+++ Treino_provas_antigas/ControleDeReservaHotel.java
@@ -0,0 +1,20 @@
+package Treino_provas_antigas;
+
+import java.util.List;
+
+public interface ControleDeReservaHotel {
+    void adicionarReserva(String nome, String dataEntrada, String dataSaida, String servicosAdicionais);
+
+    void removerReserva(int posicao); // uma vez que é uma lista de reservas
+
+    void atualizarReserva(String nome, String dataEntrada, String dataSaida, String novosServicosAdicionais);
+
+    Object[] listarReservas(); 
+
+    Object buscarReserva(int posicao); 
+
+    boolean estahVazia();
+
+    boolean estahCheia();
+    
+}
