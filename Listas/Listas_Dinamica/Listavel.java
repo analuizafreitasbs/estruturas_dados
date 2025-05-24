1a570c1359860602f9f0b1659b4751839f87a8b8
analuizafreitasbs <sanaluizafreitas@gmail.com>
2025-05-23 21:21:40 -0300

    first commit

--- Listas/Listas_Dinamica/Listavel.java
+++ Listas/Listas_Dinamica/Listavel.java
@@ -1,4 +1,38 @@
 package Listas.Listas_Dinamica;
 
 public class Listavel {
+    private Object[] elementos;
+    private int tamanho;
+    private int capacidade;
+
+    public Listavel(int capacidade) {
+        this.capacidade = capacidade;
+        this.elementos = new Object[capacidade];
+        this.tamanho = 0;
+    }
+
+    public void adicionar(Object elemento) {
+        if (tamanho == capacidade) {
+            aumentarCapacidade();
+        }
+        elementos[tamanho++] = elemento;
+    }
+
+    private void aumentarCapacidade() {
+        capacidade *= 2;
+        Object[] novosElementos = new Object[capacidade];
+        System.arraycopy(elementos, 0, novosElementos, 0, tamanho);
+        elementos = novosElementos;
+    }
+
+    public int getTamanho() {
+        return tamanho;
+    }
+
+    public Object getElemento(int index) {
+        if (index < 0 || index >= tamanho) {
+            throw new IndexOutOfBoundsException("Índice fora dos limites: " + index);
+        }
+        return elementos[index];
+    }
 }
